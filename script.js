2ba30af84abc49427db638ea6ffffbf86e06c1a0
cuadroslozada.6 <119822144+israelcuadros@users.noreply.github.com>
2024-10-01 07:03:25 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const menuToggle=document.querySelector('.menuToggle');
-const bot3d = document.getElementById('bot3d')
+const bot3d=document.getElementById('bot3d')
 
 window.addEventListener('load',iniciar)
+
 
 menuToggle.onclick = function()
 {
     menuToggle.classList.toggle('active')
+   /*  bot3d.style.display = 'block' */
 }
 
 function iniciar() {
-    bot3d.style.display = 'none'
-    mostrarBot()
-}
+    document.getElementById.bot3d.style.display = 'none'
+} 
 
-function mostrarBot() {
-    if (menuToggle == 'active') {
-        bot3d.style.display = 'block'
+
+function mostrarbot() {
+    if (menuToggle.classList.toggle('active')) {
+        document.getElementById.bot3d.style.display = 'block'
     }
+} 
+
+function ocultar()
+{
+    document.getElementById("imagen").style.display="block";
+}
+function ocultar1()
+{
+    document.getElementById("imagen").style.display="none";
 }
